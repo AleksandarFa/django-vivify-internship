ac62f59dca301493488ee7211945e88a52737463
AleksandarFa <aleksandar.fa@vivifyideas.com>
2021-05-07 17:20:35 +0200

    Added urls for all methods

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import HomeView, UserView, UserViewSet, DetailTodoView
+from .views import HomeView, UserView, UserViewSet, DetailTodoView, PostTodoView, UpdateTodoView, DeleteTodoView
 from rest_framework.routers import SimpleRouter
 
 todo_urlpatterns = [
@@ -8,5 +8,8 @@ todo_urlpatterns = [
     path("todos/", HomeView.as_view()),
     path("users/<int:pk>/", UserViewSet.as_view({'get': 'retrieve'})),
     path("users/", UserView.as_view()),
+    path("todos/post/", PostTodoView.as_view()),
+    path("todos/update/<int:pk>/", UpdateTodoView.as_view({"put":"update", "patch":"partial_update"})),
+    path("todos/delete/<int:pk>/", DeleteTodoView.as_view())
     
 ]
